9a70b59b81954a007ede5ecbd8a1731102ab78c4
KevinRichardDev <kevin.richard0@orange.fr>
2024-02-09 05:57:42 +0100

    parcourir un objet

--- cours/index.js
+++ cours/index.js
@@ -1,16 +1,24 @@
 const obj = {
   // index:value
   pseudo: "Ricke",
-  ville: "Privas"
-}
+  ville: "Privas",
+};
 
 // Ajouter
-obj.age = 35
+obj.age = 35;
 
 // Modifier
-obj.pseudo = "RK"
+obj.pseudo = "RK";
 
 // Supprimer
-delete obj.ville
-console.log(obj);
+delete obj.ville;
+// console.log(obj);
 
+// Checker si propriété existe
+// console.log("pseudo" in obj);
+// console.log("ville" in obj);
+
+//Parcourir l'objet
+for (const key in obj) {
+  console.log(key);
+}
