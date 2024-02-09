9ef12be3ff08b1f3ba3532e816f59e3ec6945454
KevinRichardDev <kevin.richard0@orange.fr>
2024-02-09 05:49:23 +0100

    Manipulation et modification d'objets

---
+++ cours/index.js
@@ -0,0 +1,16 @@
+const obj = {
+  // index:value
+  pseudo: "Ricke",
+  ville: "Privas"
+}
+
+// Ajouter
+obj.age = 35
+
+// Modifier
+obj.pseudo = "RK"
+
+// Supprimer
+delete obj.ville
+console.log(obj);
+
